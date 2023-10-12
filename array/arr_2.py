16af0212158c181045856a7ecdd134c320046373
Null <kelanidarasimi9@gmail.com>
2023-10-12 22:11:37 +0100

    array module

---
+++ array/arr_2.py
@@ -0,0 +1,6 @@
+#inserting element to an array
+import array
+
+my_array1 = array.array('i', [1, 2 ,3, 4])
+my_array1.insert(0, 6)
+print(my_array1)
---
+++ array/arr_5.py
@@ -0,0 +1,5 @@
+#deleting an element in array
+import array
+arr1 = array.array('i', [1, 2, 3, 4, 5, 6])
+arr1.remove(3)
+print(arr1)
