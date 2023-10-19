d60a54269c352d3f2d768feb470d64c62a69ab34
Null <kelanidarasimi9@gmail.com>
2023-10-19 23:28:39 +0100

    learning dict method 2

--- dictionary/dict_method.py
+++ dictionary/dict_method.py
@@ -27,3 +27,34 @@ print(my_dict)
 # update method
 my_dict.update({'a':'b','c':'d'})
 print(my_dict)
+
+# all function
+my_dict1 = {
+
+        -1: "three",
+        5: "five",
+        2: 'two',
+        3: 'three',
+        6: 'six',
+        }
+# if dict contains 0 return false
+# if dict contain False return False
+# if dict is not 0 return true
+# if dict is not false return true
+print(all(my_dict1))
+
+# any function
+
+my_dict1 = {
+
+        0: "three",
+        0: "five",
+        0: 'two',
+        0: 'three',
+        True: 'six',
+        }
+# if dict contains number > 0 or 0 will return true
+# if dict contains only key 0 will return false
+# if dict does not contain true will return false
+# if dict contains contain true will return true
+print(any(my_dict1), 'any')
