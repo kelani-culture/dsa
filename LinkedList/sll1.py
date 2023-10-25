de2dd61e37e53cba694a24212a00fc1c0e2be370
Null <kelanidarasimi9@gmail.com>
2023-10-25 14:00:41 +0100

    insert at any index

--- LinkedList/sll1.py
+++ LinkedList/sll1.py
@@ -35,6 +35,25 @@ class LinkedList:
             self.head = self.tail
         self.length +=1
 
+    def insert_at_idx(self, value, idx):
+        new_node = Node(value)
+        
+        if idx < 0 and idx > self.length:
+            return -1
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        if idx == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            temp_node = self.head
+            for _ in range(idx-1):
+                temp_node = temp_node.next
+            new_node.next = temp_node.next
+            temp_node.next = new_node
+        self.length += 1
+
     def __str__(self):
         # print element of a linked list
         temp_node = self.head
@@ -51,4 +70,10 @@ new_linked_list.append(10)
 new_linked_list.append(20)
 new_linked_list.append(30)
 new_linked_list.add_to_beginning_node(5)
-print(new_linked_list)
+
+list1 = LinkedList()
+list1.add_to_beginning_node(1)
+list1.add_to_beginning_node(2)
+list1.add_to_beginning_node(3)
+list1.insert_at_idx(4, 0)
+print(list1)
