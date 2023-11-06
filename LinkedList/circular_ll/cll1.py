56593a34be69b663ca39317b70dc54de2efaae9f
Null <kelanidarasimi9@gmail.com>
2023-11-06 16:17:17 +0100

    add the get search and traverse method to the object

--- LinkedList/circular_ll/cll1.py
+++ LinkedList/circular_ll/cll1.py
@@ -5,6 +5,8 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+    def __str__(self):
+        return str(self.value)
 
 class CSLinkedList:
     # def __init__(self, value):
@@ -18,38 +20,37 @@ class CSLinkedList:
         self.head = None
         self.tail = None
         self.length = 0
- 
+
     def append(self, value):
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
             self.tail = new_node
             new_node.next = new_node
-        
+
         else:
             self.tail.next = new_node
             self.tail = new_node
             new_node.next = self.head
         self.length += 1
-    
+
     def prepend(self, value):
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
             self.tail = new_node
             new_node.next = new_node
-            
+
         else:
             new_node.next = self.head
             self.head = new_node
             self.tail.next = new_node
         self.length += 1
 
-    def insert(self, value, idx):
+    def insert(self, value, idx=0):
+        if idx > self.length:
+            raise IndexError("Index out of range")
         new_node = Node(value)
-        
-        if idx >= self.length:
-            return -1
         if idx == 0:
             if self.head is None:
                 self.head = new_node
@@ -59,12 +60,11 @@ class CSLinkedList:
                 new_node.next = self.head
                 self.head = new_node
                 self.tail.next = self.head
-    
         elif self.length - 1 == idx:
             self.tail.next = new_node
             self.tail = new_node
             self.tail.next = self.head
-        
+ 
         else:
             temp_node = self.head
             for _ in range(idx -1):
@@ -72,7 +72,47 @@ class CSLinkedList:
                 
             new_node.next = temp_node.next
             temp_node.next = new_node
+        self.length += 1
 
+    def traverse(self):
+        current = self.head
+        while current is not None:
+            print(current.value)
+            current = current.next
+            
+    def search(self, value):
+        if self.head is  None:
+            return None
+        
+        if self.head.value == value:
+            return True
+
+        current = self.head
+        while current is not None:
+            if current.value == value:
+                return True
+
+            current = current.next
+            if current == self.head:
+                break
+        return False
+    
+    def get(self, idx):
+        """
+        get element in a linked list
+        """
+        if idx < -1 or idx > self.length:
+            return -1
+        if self.head is None:
+            return None
+        
+        current = self.head
+        for _ in range(idx):
+            current = current.next
+        return current
+    
+    def set():
+        pass
     def __str__(self):
         circle = self.head
         res = ""
@@ -83,12 +123,16 @@ class CSLinkedList:
                 break
             res += '--->'
         return res
+
     
 
 csl = CSLinkedList()
-csl.prepend(10)
-csl.prepend(5)
-csl.prepend(3)
-csl.prepend(1)
-csl.insert(2, 3)
+csl.append(10)
+csl.append(5)
+csl.append(3)
+csl.append(1)
+csl.insert(2, 0)
+csl.insert(3)
 print(csl)
+#print(csl.search(5))
+print(csl.get(2))
