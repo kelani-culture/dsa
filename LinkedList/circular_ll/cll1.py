25f9d6b0b57c541ab724c1e4b9105b7f702ef7a9
Null <kelanidarasimi9@gmail.com>
2023-11-06 12:31:48 +0100

    prepending and inserting to a circular linkked list

--- LinkedList/circular_ll/cll1.py
+++ LinkedList/circular_ll/cll1.py
@@ -31,7 +31,48 @@ class CSLinkedList:
             self.tail = new_node
             new_node.next = self.head
         self.length += 1
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            new_node.next = new_node
+            
+        else:
+            new_node.next = self.head
+            self.head = new_node
+            self.tail.next = new_node
+        self.length += 1
+
+    def insert(self, value, idx):
+        new_node = Node(value)
         
+        if idx >= self.length:
+            return -1
+        if idx == 0:
+            if self.head is None:
+                self.head = new_node
+                self.tail = new_node
+                new_node.next = new_node
+            else:
+                new_node.next = self.head
+                self.head = new_node
+                self.tail.next = self.head
+    
+        elif self.length - 1 == idx:
+            self.tail.next = new_node
+            self.tail = new_node
+            self.tail.next = self.head
+        
+        else:
+            temp_node = self.head
+            for _ in range(idx -1):
+                temp_node = temp_node.next
+                
+            new_node.next = temp_node.next
+            temp_node.next = new_node
+
     def __str__(self):
         circle = self.head
         res = ""
@@ -41,13 +82,13 @@ class CSLinkedList:
             if circle is self.head:
                 break
             res += '--->'
-            
         return res
     
 
 csl = CSLinkedList()
-csl.append(10)
-csl.append(5)
-csl.append(3)
-csl.append(1)
+csl.prepend(10)
+csl.prepend(5)
+csl.prepend(3)
+csl.prepend(1)
+csl.insert(2, 3)
 print(csl)
